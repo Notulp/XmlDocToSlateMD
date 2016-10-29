a1318497f939ea40ba945b4e0c0311017728ab0f
Unknown <balugyanszky.gabor@gmail.com>
2016-10-29 16:16:32 +0100

    ToStream extension method

---
+++ StringEx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace XmlToSlateMD
+{
+    public static class StringEx
+    {
+        public static Stream ToStream(this string self, Encoding encoding = null)
+        {
+            return new MemoryStream((encoding ?? Encoding.UTF8).GetBytes(self ?? ""));
+        }
+    }
+}
+
