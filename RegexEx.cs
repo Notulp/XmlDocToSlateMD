846dbae799d69f8c54e3c37798a1da996c854b64
Unknown <balugyanszky.gabor@gmail.com>
2016-11-01 17:59:32 +0000

    CaptureCollection.Join() extension method

---
+++ RegexEx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace XmlToSlateMD
+{
+    public static class RegexEx
+    {
+        public static string Join(this CaptureCollection self)
+        {
+            var result = "";
+            foreach (Capture capture in self)
+                result += capture.Value;
+
+            return result;
+        }
+    }
+}
+
