6aee2f72a71f1277c626b1fc9b6c57035f828375
Unknown <balugyanszky.gabor@gmail.com>
2016-11-06 17:24:11 +0000

    GetTypeName

--- StringEx.cs
+++ StringEx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace XmlToSlateMD
 {
@@ -11,6 +12,14 @@ namespace XmlToSlateMD
             if (!methodOrPropOrFieldName.Contains("("))
                 return methodOrPropOrFieldName.Substring(methodOrPropOrFieldName.LastIndexOf('.') + 1);
             return Regex.Match(methodOrPropOrFieldName, "\\.([#A-z]*)\\(").Groups[1].Captures.Join();
+        }
+
+        public static string GetTypeName(this string methodOrPropOrFieldName)
+        {
+            if (!methodOrPropOrFieldName.Contains("("))
+                return methodOrPropOrFieldName.Substring(2, methodOrPropOrFieldName.LastIndexOf(".") - 2);
+            string typename = methodOrPropOrFieldName.Substring(0, methodOrPropOrFieldName.IndexOf("("));
+            return typename.Substring(2, typename.LastIndexOf('.') - 2);
         }
 
         public static Stream ToStream(this string self, Encoding encoding = null)
