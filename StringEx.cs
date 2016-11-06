d473ca569458005566fff86fabdd132a0600fccc
Unknown <balugyanszky.gabor@gmail.com>
2016-11-06 17:23:14 +0000

    GetMemberName method

--- StringEx.cs
+++ StringEx.cs
@@ -6,9 +6,22 @@ namespace XmlToSlateMD
 {
     public static class StringEx
     {
+        public static string GetMemberName(this string methodOrPropOrFieldName)
+        {
+            if (!methodOrPropOrFieldName.Contains("("))
+                return methodOrPropOrFieldName.Substring(methodOrPropOrFieldName.LastIndexOf('.') + 1);
+            return Regex.Match(methodOrPropOrFieldName, "\\.([#A-z]*)\\(").Groups[1].Captures.Join();
+        }
+
         public static Stream ToStream(this string self, Encoding encoding = null)
         {
             return new MemoryStream((encoding ?? Encoding.UTF8).GetBytes(self ?? ""));
+        }
+
+        public static string WrapInHref(this string self)
+        {
+            var linkedname = self.Replace('.', '-').ToLowerInvariant();
+            return $"<a href=\"#{linkedname}\">{self.GetMemberName()}</a>";
         }
     }
 }
